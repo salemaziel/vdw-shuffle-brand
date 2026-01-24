72aa27a2df0a4f0673484467cb75483308c2a99c
salemaziel <51006863+salemaziel@users.noreply.github.com>
2026-01-24 23:59:26 +0000

    Published from Shuffle Editor

---
+++ src/components/__structures/IndexSection__structures16.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IndexSection__structures16: React.FC = () => {
+    return (
+        <div />
+
+
+    );
+};
+
+export default IndexSection__structures16;
---
+++ src/pages/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Head from 'next/head';
+import Script from 'next/script';
+import IndexSectionNavigations2 from '../components/navigations/IndexSectionNavigations2';
+import IndexSection__structures3 from '../components/__structures/IndexSection__structures3';
+import IndexSection__structures16 from '../components/__structures/IndexSection__structures16';
+import IndexSectionLogoClouds4 from '../components/logo-clouds/IndexSectionLogoClouds4';
+import IndexSection__structures5 from '../components/__structures/IndexSection__structures5';
+import IndexSection__structures17 from '../components/__structures/IndexSection__structures17';
+import IndexSection__structures7 from '../components/__structures/IndexSection__structures7';
+import IndexSection__structures8 from '../components/__structures/IndexSection__structures8';
+import IndexSection__structures9 from '../components/__structures/IndexSection__structures9';
+import IndexSection__structures10 from '../components/__structures/IndexSection__structures10';
+import IndexSectionTestimonials11 from '../components/testimonials/IndexSectionTestimonials11';
+import IndexSectionFaqs12 from '../components/faqs/IndexSectionFaqs12';
+import IndexSectionTeam13 from '../components/team/IndexSectionTeam13';
+import IndexSectionFooters14 from '../components/footers/IndexSectionFooters14';
+
+const Index: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title></title>
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/shuffle-for-bootstrap.png'
+        />
+      </Head>
+      <IndexSectionNavigations2 />
+      <IndexSection__structures3 />
+      <IndexSection__structures16 />
+      <IndexSectionLogoClouds4 />
+      <IndexSection__structures5 />
+      <IndexSection__structures17 />
+      <IndexSection__structures7 />
+      <IndexSection__structures8 />
+      <IndexSection__structures9 />
+      <IndexSection__structures10 />
+      <IndexSectionTestimonials11 />
+      <IndexSectionFaqs12 />
+      <IndexSectionTeam13 />
+      <IndexSectionFooters14 />
+    </>
+  );
+};
+
+export default Index;
+
